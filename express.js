8f7e98b817a5df19177ed6b379bdea1064f3dd0c
larouss <59806845+larouss@users.noreply.github.com>
2025-07-16 12:57:52 +0100

    first
    anything

---
+++ express.js
@@ -0,0 +1,47 @@
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+
+const bot = new Telegraf("8110129313:AAEALLT0fUH_-qQad2S3Vee6Yr9PVjAoIYA");
+
+bot.command("quit", async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.leaveChat(ctx.message.chat.id);
+
+  // Using context shortcut
+  await ctx.leaveChat();
+});
+
+bot.command("ping", async (ctx) => {
+  await ctx.reply(`pong`);
+});
+
+bot.on(message("text"), async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello oussama`);
+
+  // Using context shortcut
+  await ctx.reply(`Hello`);
+});
+
+bot.on("callback_query", async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
+
+  // Using context shortcut
+  await ctx.answerCbQuery();
+});
+
+bot.on("inline_query", async (ctx) => {
+  const result = [];
+  // Explicit usage
+  await ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result);
+
+  // Using context shortcut
+  await ctx.answerInlineQuery(result);
+});
+
+bot.launch();
+
+// Enable graceful stop
+// process.once('SIGINT', () => bot.stop('SIGINT'))
+// process.once('SIGTERM', () => bot.stop('SIGTERM'))
